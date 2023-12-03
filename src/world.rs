5c485dc39e9875de716d5ade1fc3ebcaa3e170a6
Carson Webster <CarsonWebster@hotmail.com>
2023-12-03 12:20:00 -0800

    Towers that shoot

--- src/world.rs
+++ src/world.rs
@@ -1,7 +1,11 @@
+use std::time::Duration;
+
+use crate::GameState;
 use bevy::prelude::*;
 use bevy_ecs_ldtk::prelude::*;
 use bevy_pancam::PanCam;
-use crate::GameState;
+
+use crate::loading::TextureAssets;
 
 // use bevy_pancam::{PanCam, PanCamPlugin};
 
@@ -21,7 +25,11 @@ impl Plugin for WorldPlugin {
             // .add_plugins(PanCamPlugin::default());
             // Register LDtk entities
             .register_ldtk_entity::<QueenBundle>("Queen")
-            .register_ldtk_entity::<EnemyQueenBundle>("EnemyQueen");
+            .register_ldtk_entity::<EnemyQueenBundle>("EnemyQueen")
+            .register_ldtk_entity::<TowerBundle>("Tower")
+            .add_systems(OnEnter(GameState::Playing), setup_tower_shooting)
+            .add_systems(Update, tower_shoot.run_if(in_state(GameState::Playing)))
+            .add_systems(Update, bullet_movement.run_if(in_state(GameState::Playing)));
     }
 }
 
@@ -67,3 +75,78 @@ struct EnemyQueenBundle {
     #[sprite_sheet_bundle]
     sprite_sheet_bundle: SpriteSheetBundle,
 }
+
+#[derive(Default, Component)]
+struct Tower;
+
+#[derive(Default, Bundle, LdtkEntity)]
+struct TowerBundle {
+    tower: Tower,
+    #[sprite_sheet_bundle]
+    sprite_sheet_bundle: SpriteSheetBundle,
+}
+
+#[derive(Default, Component)]
+struct Bullet;
+
+#[derive(Resource)]
+struct TowerShootTimer {
+    // How often to spawn a bullet (repeating timer)
+    timer: Timer,
+}
+
+// Shoot bullets from towers
+fn tower_shoot(
+    mut commands: Commands,
+    time: Res<Time>,
+    query: Query<&Transform, With<Tower>>,
+    mut bullet_timer: ResMut<TowerShootTimer>,
+    textures: Res<TextureAssets>,
+) {
+    bullet_timer.timer.tick(time.delta());
+
+    if bullet_timer.timer.finished() {
+        for transform in &mut query.iter() {
+            commands.spawn((
+                SpriteSheetBundle {
+                    texture_atlas: textures.shmup.clone(),
+                    sprite: TextureAtlasSprite::new(0),
+                    transform: Transform::from_xyz(
+                        transform.translation.x,
+                        transform.translation.y,
+                        3.0,
+                    ),
+                    ..default()
+                },
+                Bullet,
+            ));
+        }
+    }
+}
+
+// Enable tower shooting
+fn setup_tower_shooting(mut commands: Commands) {
+    commands.insert_resource(TowerShootTimer {
+        timer: Timer::new(Duration::from_secs(3), TimerMode::Repeating),
+    });
+}
+
+// Move bullets
+fn bullet_movement(
+    mut commands: Commands,
+    time: Res<Time>,
+    mut bullet: Query<(Entity, &mut Transform), With<Bullet>>,
+) {
+    // Despawn Y level
+    let despawn_y_level = 300.0;
+
+    for (entity, mut transform) in bullet.iter_mut() {
+        transform.translation.y += time.delta_seconds() * 30.0;
+
+        if transform.translation.y > despawn_y_level {
+            commands.entity(entity).despawn();
+            info!("Despawn bullet")
+        }
+    }
+}
+
