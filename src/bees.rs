13b7eddbcd75213edaf217e0ca1bc3071a4bb3ab
Carson Webster <CarsonWebster@hotmail.com>
2023-12-04 18:16:18 -0800

    Random direction and rotation

--- src/bees.rs
+++ src/bees.rs
@@ -14,8 +14,8 @@ impl Plugin for BeesPlugin {
         app.add_systems(OnEnter(GameState::Playing), setup)
             .add_systems(Update, update_cursor.run_if(in_state(GameState::Playing)))
             .add_systems(Update, place_bee.run_if(in_state(GameState::Playing)))
-            .add_systems(Update,show_mouse_location.run_if(in_state(GameState::Playing)));
-            // .add_systems(Update, move_bee.run_if(in_state(GameState::Playing)));
+            .add_systems(Update,show_mouse_location.run_if(in_state(GameState::Playing)))
+            .add_systems(Update, move_bee.run_if(in_state(GameState::Playing)));
     }
 }
 
@@ -34,18 +34,19 @@ struct Velocity(Vec3);
 impl Velocity {
     pub fn default() -> Self {
         let mut rng = rand::thread_rng();
-        Velocity(Vec3::new(rng.gen_range(1.0..20.0), rng.gen_range(1.0..20.0), 0.0))
+        Velocity(Vec3::new(rng.gen_range(-20.0..20.0), rng.gen_range(-20.0..20.0), 0.0))
     }
 }
 
 #[derive(Component)]
 struct Bee;
 
-// #[derive(Resource)]
-// struct BeeSpawner {
-//     // How often to spawn a bee (repeating timer)
-//     timer: Timer,
-// }
+#[derive(Component)]
+struct Boid {
+    seperation: f32,
+    alignment: f32,
+    cohesion: f32,
+}
 
 #[derive(Resource)]
 struct MousePosition {
@@ -60,34 +61,6 @@ fn setup(mut commands: Commands) {
         position: Vec2::new(0.0, 0.0),
     });
 }
-
-// fn spawn_bee(
-//     mut commands: Commands,
-//     time: Res<Time>,
-//     mut bee_spawner: ResMut<BeeSpawner>,
-//     textures: Res<TextureAssets>,
-//     query: Query<&Transform, With<Queen>>,
-// ) {
-//     bee_spawner.timer.tick(time.delta());
-
-//     if bee_spawner.timer.just_finished() {
-//         let queen_transform = query.single();
-//         commands.spawn((
-//             SpriteSheetBundle {
-//                 texture_atlas: textures.planes.clone(),
-//                 sprite: TextureAtlasSprite::new(11),
-//                 transform: Transform::from_xyz(
-//                     queen_transform.translation.x,
-//                     queen_transform.translation.y,
-//                     5.0,
-//                 ),
-//                 ..default()
-//             },
-//             Bee,
-//         ));
-//         info!("Bee spawned");
-//     }
-// }
 
 // Update mouse position
 fn update_cursor(
@@ -127,46 +100,24 @@ fn place_bee(
                 },
                 Bee,
                 Collider::new(5.0),
+                Velocity::default(),
 
             ));
             info!("Bee spawned");
     }
 }
 
-// Move bees toards mouse cursor
-// fn move_bee(
-//     mut query: Query<(&mut Transform, &mut TextureAtlasSprite), With<Bee>>,
-//     mouse_position: Res<MousePosition>,
-//     time: Res<Time>,
-// ) {
-//     let speed = 25.0;
-//     for (mut transform, mut sprite) in query.iter_mut() {
-//         let mut direction = mouse_position.position - transform.translation.truncate();
-//         direction = direction.normalize();
-//         // info!("Direction: {:?}", direction);
-//         transform.translation +=
-//             Vec3::new(direction.x, direction.y, 0.0) * speed * time.delta_seconds();
-//         if direction.x > 0.0 {
-//             sprite.flip_x = false;
-//         } else {
-//             sprite.flip_x = true;
-//         }
-//     }
-// }
-
 // Move bees according to velocity
 fn move_bee(
-    mut query: Query<(&mut Transform, &mut TextureAtlasSprite, &mut Velocity), With<Bee>>,
+    mut query: Query<(&mut Transform, &mut Velocity), With<Bee>>,
     time: Res<Time>,
 ) {
-    let speed = 25.0;
-    for (mut transform, mut sprite, mut velocity) in query.iter_mut() {
-        transform.translation += velocity * speed * time.delta_seconds();
-        if velocity.x > 0.0 {
-            sprite.flip_x = false;
-        } else {
-            sprite.flip_x = true;
-        }
+    // let speed = 25.0;
+    for (mut transform, velocity) in query.iter_mut() {
+        transform.translation.x += velocity.0.x * time.delta_seconds();
+        transform.translation.y += velocity.0.y * time.delta_seconds();
+
+        transform.rotation = Quat::from_rotation_arc(Vec3::Y, velocity.0.normalize());
     }
 }
 
